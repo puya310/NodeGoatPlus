214b1e118e00e5c9860a03dcfd1fa28ae97fc13a
puya310 <puya310r@gmail.com>
2025-02-26 10:00:20 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ var exec = require("child_process").exec;
 var JS_FILES = ["Gruntfile.js", "app/assets/js/**", "config/config.js", "app/data/**/*.js",
     "app/routes/**/*.js", "server.js", "test/**/*.js"
 ];
-
+ 
 
 module.exports = function(grunt) {
     // Project Configuration
